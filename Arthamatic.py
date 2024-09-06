7be9bf5ac9118ca9e8f4dc4568f25a4868587047
arif-900 <https://github.com/arif-900/java.git>
2024-09-06 22:06:08 +0530

    Arthamatic operators

---
+++ Arthamatic.py
@@ -0,0 +1,7 @@
+a=10
+b=20
+print("Addition of two numbers:",(a+b))
+print("Substraction of two numbers:",(a-b))
+print("Multiplication of two numbers:",(a*b))
+print("Division of two numbers:",(a/b))
+print("Mod division of two numbers:",(a%b))
